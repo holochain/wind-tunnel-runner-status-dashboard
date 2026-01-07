d78b13198ab2618546da1635a9f6b27e27643253
Matt <matt@buildyourweb.app>
2026-01-07 11:43:04 -0700

    feat: sort clients by name, note last updated time

--- src/routes.rs
+++ src/routes.rs
@@ -2,14 +2,20 @@ use axum::extract::State;
 use axum:: response::Html;
 use crate::AppState;
 use std::sync::Arc;
+use chrono::Utc;
 
 
 pub(crate) async fn list_clients(
     State(state): State<Arc<AppState>>,
 ) -> Html<String> {
-    let cache = state.clients.read().expect("Poisoned");  
+    let clients = state.clients.read().expect("Poisoned");  
+    let last_updated = state.last_updated.read().expect("Poisoned");
+    let last_updated_s_ago = Utc::now().signed_duration_since(*last_updated).num_seconds();
+    
+    let mut clients_list: Vec<_> = clients.clone().into_iter().collect();
+    clients_list.sort_by_key(|(k, _)| k.clone());
 
-    let list_items = cache.iter()
+    let clients_list_html = clients_list.iter()
         .map(|(k,v)| format!("<li><strong>{k}</strong>: {v}</li>"))
         .collect::<Vec<String>>()
         .join("\n");
@@ -23,8 +29,9 @@ pub(crate) async fn list_clients(
             </head>
             <body>
                 <h1>Wind Tunnel Runners</h1>
-                <ul>{list_items}<ul>
+                <h5>Last Updated {last_updated_s_ago} seconds ago. Updated every {} seconds.</h5>
+                <ul>{clients_list_html}<ul>
             </body>
         </html>
-    "#,))
+    "#, state.update_seconds))
 }
